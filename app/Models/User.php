e0b691878450a50ed9b47f0512a41bf5c4d4fb17
Ramy <leguiquecchic@gmail.com>
2020-11-24 23:45:29 +0100

    install laravel-mongodb package & configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Notifications\Notifiable;
+
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
+
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
