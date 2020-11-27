78a675c915a9aa39540ac2bca083cfce4c7b7477
Ramy <leguiquecchic@gmail.com>
2020-11-27 13:57:34 +0100

    fixed title input bug

--- resources/js/components/Todo.js
+++ resources/js/components/Todo.js
@@ -103,13 +103,14 @@ class Todo extends React.Component {
                         <input
                             type="text" className="form-control mb-2 mr-sm-2"
                             id="title" placeholder="Titre de la tâche"
+                            value={this.state.title}
                             onChange={this.handleChange}/>
 
                         <button type="submit" className="btn btn-primary mb-2">
-                            <span class="btn-label">
+                            <span className="btn-label">
                                 <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-plus"
                                      fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                  <path fill-rule="evenodd"
+                                  <path fillRule="evenodd"
                                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                                 </svg>
                                 Ajouter
